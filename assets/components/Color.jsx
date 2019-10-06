7b6c7ab9718c6eaef23cce3f13df63ab6051fff9
Bram van Osta <bramvanosta@gmail.com>
2019-10-07 00:16:57 +0200

    :lipstick: Updated colors

--- assets/components/Color.jsx
+++ assets/components/Color.jsx
@@ -22,10 +22,35 @@ const Name = styled.p`
 `;
 
 function Color(props) {
+  function luminanace(r, g, b) {
+    var a = [r, g, b].map(function(v) {
+      v /= 255;
+      return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+    });
+    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+  }
+
+  function contrast(rgb1, rgb2) {
+    return (
+      (luminanace(rgb1[0], rgb1[1], rgb1[2]) + 0.05) /
+      (luminanace(rgb2[0], rgb2[1], rgb2[2]) + 0.05)
+    );
+  }
+
+  function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    return result
+      ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)]
+      : null;
+  }
+
+  const colorContrast = contrast([255, 255, 255], hexToRgb(props.default));
+
   return (
     <div>
       <ColorBox color={props.default}>
-        <ColorValue light={props.isLight}>{props.default}</ColorValue>
+        <ColorValue light={colorContrast < 3}>{props.default}</ColorValue>
       </ColorBox>
       <Name>{props.name}</Name>
     </div>
--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,15 +1,23 @@
 const colors: { [key: string]: string } = {
   '--color-primary': '#153285',
-  '--color-primary--hover': '#0d2875',
+  '--color-primary--hover': '#214ecc',
+
+  '--color-secondary': '#4d7cff',
+  '--color-secondary--hover': '#3d63cc',
 
   '--color-danger': '#ef626c',
   '--color-danger--hover': '#e1515c',
   '--color-danger--light': '#fef6f6',
 
+  '--color-error': '#ef626c',
+
   '--color-grey': '#4a4a4a',
-  '--color-grey--light': '#898989',
+  '--color-grey--light': '#b9b9b9',
+
+  '--color-grey-border': '#e5e6e6',
 
   '--color-text': '#333333',
+  '--color-text-grey': '#616c7a',
 
   '--color-background-grey': '#f5f6fa',
 };
